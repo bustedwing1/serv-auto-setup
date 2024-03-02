8762b49b2d0cd25ab1007a545b133a711bec3d32
Busted Wing <bustedwing1@gmail.com>
2024-03-02 10:30:01 -0500

    BeagleV-Fire automation tweeks

--- beaglev-fire/build.sh
+++ beaglev-fire/build.sh
@@ -2,13 +2,14 @@
 ## Run this script from SERV's workspace
 
 sed -i 's/VERILOG_TEMPLATE/MY_SERV_CAPE/g' ../beaglev-fire/my_custom_fpga_design.yaml
+rm -rf ../../sources/FPGA-design/script_support/components/CAPE/MY_SERV_CAPE
 cp -rf ../../sources/FPGA-design/script_support/components/CAPE/VERILOG_TEMPLATE ../../sources/FPGA-design/script_support/components/CAPE/MY_SERV_CAPE
 cp ../beaglev-fire/ADD_CAPE.tcl  ../../sources/FPGA-design/script_support/components/CAPE/MY_SERV_CAPE/ADD_CAPE.tcl
 sed -i 's/VERILOG_TEMPLATE/MY-SERV_CAPE/g' ../../sources/FPGA-design/script_support/components/CAPE/MY_SERV_CAPE/device-tree-overlay/verilog-cape.dtso
 
 sed -i 's#zephyr_hello.hex#../../blinky.hex#' fusesoc_libraries/serv/servant/servant.v
 sed -i 's#`default_nettype#// `default_nettype#' fusesoc_libraries/serv/servant/*.v
-verilator_blinky
+verilator_blinky --timeout 1000
 
 cp -rf build/servant_1.2.1/verilator_tb/src/serv_1.2.1 ../../sources/FPGA-design/script_support/components/CAPE/MY_SERV_CAPE/HDL
 cp -rf build/servant_1.2.1/verilator_tb/src/servant_1.2.1 ../../sources/FPGA-design/script_support/components/CAPE/MY_SERV_CAPE/HDL
